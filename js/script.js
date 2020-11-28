04c6d4266fa9b9f400e2cb71b9cfa995072b448f
Chercas <48250002+Chercsa@users.noreply.github.com>
2020-11-28 22:00:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false,
+		speed:1000,
+		autoplay:true,
+		autoplaySpeed:4000,
+		draggable:false,
+		swipe:false,
+		centerMode:true,
+		variableWidth:true,
+		initialSlide:2,
+		adaptiveHeight:true,
+	});
+
+});
